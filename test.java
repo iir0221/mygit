b297cee260b92d302b49df13cb9c3f486d60f870
iir0221 <iir0221@163.com>
2014-10-17 10:23:58 +0800

    change

--- test.java
+++ test.java
@@ -18,11 +18,10 @@ public class InfoDemo01 {
 	public static void main(String[] args) throws InterruptedException {
 		MyThread it =new MyThread();
 		Thread proxy =new Thread(it,"挨踢");
-		//一行测试
 		proxy.setName("test");
 		System.out.println(proxy.getName());
 		System.out.println(Thread.currentThread().getName()); //main
-		
+		//加入一行
 		proxy.start();
 		System.out.println("启动后的状态:"+proxy.isAlive());
 		Thread.sleep(200);
